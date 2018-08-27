78b3a320e29fe46309c046d3f8f12e835774ab12
Giorgi Nikolaishvili <giorgi.nikolaishvili25@gmail.com>
2018-08-27 13:18:45 +0400

    add user & stock assotiation

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,4 +1,11 @@
 class Stock < ApplicationRecord
+  has_many :user_stocks
+  has_many :users, through: :user_stocks
+
+  def self.find_by_ticker(ticker)
+    where(ticker: ticker).first
+  end
+
   def self.new_from_lookup(ticker_symbol)
     begin
       looked_up_stock = StockQuote::Stock.quote(ticker_symbol)
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,20 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_many :user_stocks
+  has_many :stocks, through: :user_stocks
+
+  def stock_already_added_to_portfolio?(ticker)
+    stock = Stock.find_by_ticker(ticker)
+    return false unless stock
+    user_stocks.where(stock_id: stock.id).exists?
+  end
+
+  def under_stock_limit?
+    (user_stocks.count < 10)
+  end
+
+  def can_add_stock?(ticker)
+    under_stock_limit? && !stock_already_added_to_portfolio?(ticker)
+  end
 end
